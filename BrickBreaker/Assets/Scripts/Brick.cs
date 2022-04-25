0a7f3938b55fa3d7b891facc56c68dc17091a79e
darothmedia <87622162+darothmedia@users.noreply.github.com>
2022-04-25 00:09:17 -0700

    add win state

--- BrickBreaker/Assets/Scripts/Brick.cs
+++ BrickBreaker/Assets/Scripts/Brick.cs
@@ -4,28 +4,45 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour
 {
-    public int MaxHits;
+    public Sprite[] hitSprites;
+    public static int breakableCount = 0;
     private int timesHit;
+    private bool isBreakable;
     private LevelManager levelManager;
     // Start is called before the first frame update
     void Start()
     {
+        isBreakable = (this.tag == "Breakable");
+        if (isBreakable){
+            breakableCount++;
+        }
         timesHit = 0;
         levelManager = GameObject.FindObjectOfType<LevelManager>();
     }
 
     void OnCollisionEnter2D(Collision2D collision){
-        timesHit++;
-        if (timesHit == MaxHits) {
-            Destroy(gameObject);
+        if (isBreakable) {
+            HandleHits();
         }
-        SimulateWin();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void HandleHits(){
+        int MaxHits = hitSprites.Length + 1;
+        timesHit++;
+        if (timesHit >= MaxHits) {
+            breakableCount--;
+            levelManager.BrickDestroyed();
+            Destroy(gameObject);
+        } else {
+            LoadSprites();
+        }
+    }
+
+    void LoadSprites(){
+        int spriteIndex = timesHit - 1;
+        if (hitSprites[spriteIndex]){
+            this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        } 
     }
 
     void SimulateWin(){
